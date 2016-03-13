01119fc3ccc6f523b78efd0a5a0e6a81bc50d18e
Julie <julie.alary@gmail.com>
2016-03-13 19:03:55 +0100

    ajouter page parcours

---
+++ app/assets/javascripts/certif.js
@@ -0,0 +1,18 @@
+// ci desous les codes pour les slides down_parcours OpenclssRoom //   
+$(document).ready(function(){
+    $(".flip").click(function(){
+        $(".panel").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".flip2").click(function(){
+        $(".panel2").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".flip3").click(function(){
+        $(".panel3").slideToggle("slow");
+    });
+});
+//-------------------------------------------------------------------//
+
